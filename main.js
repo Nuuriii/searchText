a17596194e91053fa0eb5d2874f567c72b555936
nurr <nurr@pop-os.localdomain>
2023-05-24 13:28:51 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+function Button() {
+   alert("Maaf Tapi tombol Button Masih Belum Bisa Dipakai");
+}
+
+let search = "Java";
+
+const list = document.querySelector(".output");
+const list2 = document.querySelector("#notSearch");
+list.innerHTML = "";
+const greetings = [
+   "Belajar Js",
+   "Halo Dunia",
+   "Javascript dari awal",
+   "Javascript DOm",
+   "Javascript Modern",
+];
+
+for (const greeting of greetings) {
+   // Your conditional test needs to go inside the parentheses
+   // in the line below, replacing what's currently there
+   if (greeting.includes(search)) {
+      const listItem = document.createElement("li");
+      listItem.textContent = greeting;
+      list.appendChild(listItem);
+   }
+}
